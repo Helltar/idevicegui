efce142cb931d4e862dad929b4eb7183b77d64ff
Helltar <iam@helltar.com>
2022-08-28 03:12:33 +0300

    update aboutForm

--- src/forms/uaboutform.pas
+++ src/forms/uaboutform.pas
@@ -5,15 +5,41 @@ unit uAboutForm;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Forms, ExtCtrls, StdCtrls, LCLIntf;
 
 type
+
+  { TfrmAbout }
+
   TfrmAbout = class(TForm)
+    imgLogo: TImage;
+    lblHomepage: TLabel;
+    lblGithub: TLabel;
+    lblLicense: TLabel;
+    procedure lblHomepageClick(Sender: TObject);
+    procedure lblGithubClick(Sender: TObject);
+    procedure lblLicenseClick(Sender: TObject);
   end;
 
 implementation
 
 {$R *.lfm}
 
-end.
+{ TfrmAbout }
 
+procedure TfrmAbout.lblHomepageClick(Sender: TObject);
+begin
+  OpenURL('https://helltar.com');
+end;
+
+procedure TfrmAbout.lblGithubClick(Sender: TObject);
+begin
+  OpenURL('https://github.com/Helltar/idevicegui');
+end;
+
+procedure TfrmAbout.lblLicenseClick(Sender: TObject);
+begin
+  OpenURL('https://raw.githubusercontent.com/Helltar/idevicegui/master/LICENSE');
+end;
+
+end.
