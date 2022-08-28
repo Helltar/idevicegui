d569023ecbf97395a4b469417dbe75520f0cb28a
Helltar <iam@helltar.com>
2022-08-28 16:46:38 +0300

    ui changes

--- src/forms/umainform.pas
+++ src/forms/umainform.pas
@@ -5,7 +5,8 @@ unit uMainForm;
 interface
 
 uses
-  Classes, SysUtils, Forms, Graphics, Dialogs, StdCtrls, ExtCtrls, ActnList, LCLIntf,
+  Classes, SysUtils, Forms, Graphics, Dialogs, StdCtrls, ExtCtrls, ActnList,
+  LCLIntf, Buttons, Menus,
   //------
   uConfig;
 
@@ -17,31 +18,38 @@ type
     actMount: TAction;
     actUmount: TAction;
     actlMain: TActionList;
+    bitBtnMenu: TBitBtn;
     btnMount: TButton;
     btnUmount: TButton;
-    btnSettings: TButton;
-    btnExit: TButton;
-    btnAbout: TButton;
     gbInfo: TGroupBox;
+    lblModelNumberData: TLabel;
+    lbliOSVersionData: TLabel;
+    lblSerialNumberData: TLabel;
+    lblCycleCountData: TLabel;
+    lblSpaceUsedData: TLabel;
     lblSpaceUsed: TLabel;
-    lblDataCapacity: TLabel;
-    lblDiskCapacity: TLabel;
     lblModelNumber: TLabel;
     lbliOSVersion: TLabel;
     lblSerialNumber: TLabel;
     lblCycleCount: TLabel;
+    separator: TMenuItem;
+    miAbout: TMenuItem;
+    miSettings: TMenuItem;
+    miExit: TMenuItem;
     pnlButtons: TPanel;
+    pmMain: TPopupMenu;
     tmMain: TTimer;
     procedure actMountExecute(Sender: TObject);
     procedure actMountUpdate(Sender: TObject);
     procedure actUmountExecute(Sender: TObject);
     procedure actUmountUpdate(Sender: TObject);
-    procedure btnAboutClick(Sender: TObject);
-    procedure btnExitClick(Sender: TObject);
-    procedure btnSettingsClick(Sender: TObject);
+    procedure bitBtnMenuClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure miSettingsClick(Sender: TObject);
+    procedure miAboutClick(Sender: TObject);
+    procedure miExitClick(Sender: TObject);
     procedure tmMainTimer(Sender: TObject);
   private
     mountDir: string;
@@ -92,8 +100,31 @@ procedure TfrmMain.FormShow(Sender: TObject);
 begin
   Constraints.MinHeight := Height;
   Constraints.MinWidth := Width;
-  Constraints.MaxHeight := Height;
-  Constraints.MaxWidth := Width;
+end;
+
+procedure TfrmMain.miSettingsClick(Sender: TObject);
+begin
+  with TfrmSettings.Create(Self) do
+    try
+      ShowModal;
+    finally
+      Free;
+    end;
+end;
+
+procedure TfrmMain.miAboutClick(Sender: TObject);
+begin
+  with TfrmAbout.Create(Self) do
+    try
+      ShowModal;
+    finally
+      Free;
+    end;
+end;
+
+procedure TfrmMain.miExitClick(Sender: TObject);
+begin
+  Close;
 end;
 
 procedure TfrmMain.tmMainTimer(Sender: TObject);
@@ -106,29 +137,9 @@ begin
   actUmount.Enabled := not mountDir.IsEmpty;
 end;
 
-procedure TfrmMain.btnAboutClick(Sender: TObject);
+procedure TfrmMain.bitBtnMenuClick(Sender: TObject);
 begin
-  with TfrmAbout.Create(Self) do
-    try
-      ShowModal;
-    finally
-      Free;
-    end;
-end;
-
-procedure TfrmMain.btnExitClick(Sender: TObject);
-begin
-  Close;
-end;
-
-procedure TfrmMain.btnSettingsClick(Sender: TObject);
-begin
-  with TfrmSettings.Create(Self) do
-    try
-      ShowModal;
-    finally
-      Free;
-    end;
+  pmMain.PopUp;
 end;
 
 procedure TfrmMain.actMountUpdate(Sender: TObject);
@@ -154,23 +165,26 @@ begin
 end;
 
 procedure TfrmMain.updateControls;
-const
-  DELIMITER = ': ';
-
 begin
   if isDevicePlugged() then
   begin
     gbInfo.Enabled := True;
     gbInfo.Caption := getDeviceName();
 
-    lblModelNumber.Caption := RS_MODEL_NUMBER_CAPTION + DELIMITER + getDeviceInfoByKey('RegulatoryModelNumber');
-    lbliOSVersion.Caption := RS_OS_VERSION_CAPTION + DELIMITER + getDeviceInfoByKey('ProductVersion');
-    lblSerialNumber.Caption := RS_SERIAL_NUMBER_CAPTION + DELIMITER + getDeviceInfoByKey('SerialNumber');
-    lblCycleCount.Caption := RS_CYCLE_COUNT_CAPTION + DELIMITER + getDeviceCycleCount();
+    lblModelNumber.Caption := RS_MODEL_NUMBER_CAPTION;
+    lblModelNumberData.Caption := getDeviceInfoByKey('RegulatoryModelNumber');
 
-    lblSpaceUsed.Caption := RS_SPACE_USED + DELIMITER;
-    lblDataCapacity.Caption := formatByteSize(StrToInt64(getDiskUsage(TOTAL_DATA_CAPACITY))) + ' / ';
-    lblDiskCapacity.Caption := formatDiskCapacity(StrToInt64(getDiskUsage(TOTAL_DISK_CAPACITY)));
+    lbliOSVersion.Caption := RS_OS_VERSION_CAPTION;
+    lbliOSVersionData.Caption := getDeviceInfoByKey('ProductVersion');
+
+    lblSerialNumber.Caption := RS_SERIAL_NUMBER_CAPTION;
+    lblSerialNumberData.Caption := getDeviceInfoByKey('SerialNumber');
+
+    lblCycleCount.Caption := RS_CYCLE_COUNT_CAPTION;
+    lblCycleCountData.Caption := getDeviceCycleCount();
+
+    lblSpaceUsed.Caption := RS_SPACE_USED;
+    lblSpaceUsedData.Caption := formatByteSize(StrToInt64(getDiskUsage(TOTAL_DATA_CAPACITY))) + ' / ' + formatDiskCapacity(StrToInt64(getDiskUsage(TOTAL_DISK_CAPACITY)));
   end
   else
   begin
@@ -178,13 +192,19 @@ begin
     gbInfo.Caption := RS_GB_INFO_CAPTION;
 
     lblModelNumber.Caption := RS_MODEL_NUMBER_CAPTION;
+    lblModelNumberData.Caption := 'A2631';
+
     lbliOSVersion.Caption := RS_OS_VERSION_CAPTION;
+    lbliOSVersionData.Caption := '15.0';
+
     lblSerialNumber.Caption := RS_SERIAL_NUMBER_CAPTION;
+    lblSerialNumberData.Caption := 'XXXXXXXXXXXX';
+
     lblCycleCount.Caption := RS_CYCLE_COUNT_CAPTION;
+    lblCycleCountData.Caption := '0';
 
     lblSpaceUsed.Caption := RS_SPACE_USED;
-    lblDataCapacity.Caption := '';
-    lblDiskCapacity.Caption := '';
+    lblSpaceUsedData.Caption := '0GB / 0GB';
   end;
 end;
 
